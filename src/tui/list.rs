d8ecf2f6c4b3da61e5f689d9dfa639d91c3130e0
xulien <julien@brdb.fr>
2025-07-17 20:11:21 +0200

    refactoring tui as a module, extracting the list

---
+++ src/tui/list.rs
@@ -0,0 +1,53 @@
+use crate::chroot::ChrootUnit;
+use crate::config::Config;
+use crate::error::ChrootManagerError;
+use cursive::traits::*;
+use cursive::views::*;
+use cursive::{Cursive, CursiveExt};
+use std::process::Command;
+
+pub fn list(
+    config: &Config,
+) -> Result<ResizedView<NamedView<SelectView<ChrootUnit>>>, ChrootManagerError> {
+    let units = ChrootUnit::find_units(config)?;
+
+    let mut select = SelectView::new();
+
+    for unit in units {
+        select.add_item(unit.name.as_str(), unit.clone());
+    }
+
+    let unit_selected = select
+        .on_submit(show_unit_info)
+        .with_name("select")
+        .full_screen();
+
+    Ok(unit_selected)
+}
+
+fn show_unit_info(s: &mut Cursive, unit: &ChrootUnit) {
+    let current = unit.clone();
+    s.add_layer(
+        Dialog::text(format!(
+            "Profile: {}\nPath: {}\n",
+            unit.stage3_profile,
+            unit.chroot_path.display()
+        ))
+        .title(unit.name.to_string())
+        .button("Enter", move |s| enter_chroot(s, &current))
+        .button("Cancel", |s| {
+            s.pop_layer();
+        }),
+    )
+}
+
+fn enter_chroot(s: &mut Cursive, unit: &ChrootUnit) {
+    s.quit();
+    Command::new("reset").status().ok();
+    unit.mount_filesystems()
+        .unwrap()
+        .enter_chroot_interactive()
+        .unwrap();
+    s.pop_layer();
+    s.run();
+}
---
+++ src/tui/mod.rs
@@ -0,0 +1,24 @@
+mod list;
+
+use crate::config::Config;
+use crate::error::ChrootManagerError;
+use crate::tui::list::list;
+use cursive::views::*;
+use cursive::Cursive;
+
+pub fn run(config: &Config) -> Result<(), ChrootManagerError> {
+    let mut siv = cursive::default();
+
+    siv.add_layer(
+        Dialog::around(
+            LinearLayout::horizontal()
+                .child(list(config)?)
+                .child(DummyView)
+                .child(Button::new("Quit", Cursive::quit)),
+        )
+        .title("Select a profile"),
+    );
+
+    siv.run();
+    Ok(())
+}
